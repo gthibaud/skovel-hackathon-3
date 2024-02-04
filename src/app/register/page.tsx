7aa3795c551812fee60f4c261c216008d0e35a25
Grégoire Thibaud <gregoire.thibo@gmail.com>
2024-02-04 17:17:12 +0100

    add signin and QR ticket

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,52 @@
 'use client'
 
+import { auth, db } from '@/api/firebase';
+import { Signin } from '@/components/Signin';
 import {
     EmbeddedCheckout,
     EmbeddedCheckoutProvider
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { User } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import QRCode from "react-qr-code";
 import './styles.css';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
 export default function Register() {
+    const [user, setUser] = useState<User | null>(null);
+    const [alreadyRegistered, setAlreadyRegistered] = useState(false);
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((u) => {
+            if (u) {
+                console.log('User is logged in');
+                setUser(u);
+                const docRef = doc(db, "users", u.uid);
+                getDoc(docRef).then((doc) => {
+                    if (doc.exists()) {
+                        if (doc.data()?.registered) {
+                            setAlreadyRegistered(true);
+                        }
+                        if (doc.data()?.name) {
+                            setUserName(doc.data().name);
+                        }
+                    }
+                }).catch((error) => {
+                    console.log('Error getting document:', error);
+                });
+            } else {
+                console.log('User is logged out');
+                setUser(null);
+            }
+        });
+        return unsubscribe;
+    }, []);
+
     const [clientSecret, setClientSecret] = useState('');
 
     useEffect(() => {
@@ -25,29 +60,51 @@ export default function Register() {
     return (
         <main>
             <header>
-                <h1>Register</h1>
+                <h1>{user ? "My registration" : "Register"}</h1>
             </header>
-            <div className='already-registered'>
-                <p>Already registered?</p>
-                <Link href={"/account"} className='action register'>View my account</Link>
-            </div>
-            <div className='bento'>
-                <div className='bento-card'>
-                    <h3>Infos</h3>
-                    <p>Price: <b>150€</b></p>
-                    <p>Distance: <b>10k</b></p>
-                </div>
-            </div>
-            <div id="checkout">
-                {clientSecret && (
-                    <EmbeddedCheckoutProvider
-                        stripe={stripePromise}
-                        options={{ clientSecret }}
-                    >
-                        <EmbeddedCheckout />
-                    </EmbeddedCheckoutProvider>
-                )}
-            </div>
+            {user && alreadyRegistered ? (
+                <>
+                    <div className='already-registered'>
+                        <p>More infos</p>
+                        <Link href={"/account"} className='action register'>View my account</Link>
+                    </div>
+                    <div className='bento'>
+                        <div className='bento-card'>
+                            <h3>My race ticket</h3>
+                            <p>Name: <b>{userName}</b></p>
+                            <QRCode value={user.uid} />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className='already-registered'>
+                        <p>Already registered?</p>
+                        <Link href={"/account"} className='action register'>View my account</Link>
+                    </div>
+                    <div className='bento'>
+                        <div className='bento-card'>
+                            <h3>Infos</h3>
+                            <p>Price: <b>150€</b></p>
+                            <p>Distance: <b>10k</b></p>
+                        </div>
+                    </div>
+                    {user ? (
+                        <div id="checkout">
+                            {clientSecret && (
+                                <EmbeddedCheckoutProvider
+                                    stripe={stripePromise}
+                                    options={{ clientSecret }}
+                                >
+                                    <EmbeddedCheckout />
+                                </EmbeddedCheckoutProvider>
+                            )}
+                        </div>
+                    ) : (
+                        <Signin />
+                    )}
+                </>
+            )}
         </main>
     );
 };
